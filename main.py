4e0e071ad673c067384a170789903047f1f11c8e
Aditya Arolkar <aditya.arolkar@berkeley.edu>
2023-01-05 03:02:29 -0800

    updated search functions, and cleaned up bugs

--- main.py
+++ main.py
@@ -1,58 +1,71 @@
-# This is a sample Python script.
-
-# Press ⇧⏎ to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
+#
+# Basic python youtube downloader using pytube and ffmpeg for audio + video merging 0 - to achieve the best resolution
+#
+import time
 import ffmpeg
 from pytube import YouTube, Search
-from utils import print_dict, user_allows
+from utils import user_allows, enforce_options
 
 
 class YoutubeDownloader:
     def __init__(self, url: str = None):
-        if not url:
-            self.search()
-            # url = str(input('What youtube URL would you like to download?'))
-        self.yt = YouTube(url)
+        if url:
+            self.yt = YouTube(url)
+        else:
+            print('Would you like to input a URL for your video or search for one?')
+            user_input = enforce_options(['url', 'search'])
+            if user_input == 'url':
+                self.yt = YouTube(input('Please enter the youtube URL: '))
+            else:
+                self.yt = self.search()
 
-    def search(self, search_query: str = None):
+    def search(self, search_query: str = None, search_list_length: int = 5):
         if not search_query:
             search_query = input('What would you like to search? \n')
         s = Search(search_query)
+        video_to_downloader = {}
+        for i, r in enumerate(s.results):
+            if i+1 > search_list_length:
+                break
+            video_to_downloader[i+1] = r
+            temp_yd = YoutubeDownloader(r.watch_url)
+            print(f'[{i+1}]  -  ({temp_yd.get_best_video().resolution}, {temp_yd.get_best_audio().abr}) {r.title}')
 
-        print(len(s.results))
-        for r in s.results:
-            yd = YoutubeDownloader(url=r.watch_url)
-
-            print(f'({yd.get_best_video().resolution}, {yd.get_best_audio().abr}) [{r.title}]')
+        print('Which video do you want to download?')
+        user_input = int(enforce_options([str(k) for k in video_to_downloader.keys()]))
+        return video_to_downloader[user_input]
 
     def get_best_audio(self):
-        # get best audio bit rate
-        return self.yt.streams.filter(adaptive=True).order_by('abr').desc().first()
+        # get best average bit rate audio stream
+        return self.yt.streams.filter(only_audio=True).order_by('abr').desc().first()
 
     def get_best_video(self):
-        # get best video resolution
+        # get best resolution DASH video stream
         return self.yt.streams.filter(adaptive=True).order_by('resolution').desc().first()
 
     def download_best_resolution(self):
+        # query audio and video stream, then merge them with ffmpeg
         best_audio = self.get_best_audio()
         best_video = self.get_best_video()
 
         if user_allows(f'Would you like to download this stream?\n '
                        f'Video resolution ({best_video.resolution}): {best_video} \n '
                        f'Audio resolution ({best_audio.abr}): {best_audio}'):
+            start = time.time()
             best_audio.download(filename="temp/audio.mp3")
             best_video.download(filename="temp/video.mp4")
             audio = ffmpeg.input("temp/audio.mp3")
             video = ffmpeg.input("temp/video.mp4")
-            ffmpeg.output(audio, video, f"~/Movies/YouTube Downloads/{self.yt.title}").run(overwrite_output=True)
+            ffmpeg.output(audio, video, f"downloads/{self.yt.title}.mp4").run(overwrite_output=True)
+            end = time.time()
 
+            print('  -  DOWNLOAD COMPLETE  -  ')
+            print(f'    time taken: {round(end - start)} seconds')
 
 
 if __name__ == '__main__':
-
+    print('=' * 20 + 'PYTHON HIGH RES. YOUTUBE DOWNLOADER ' + '=' * 20)
+    # select the video in initializing the downloader
     yd = YoutubeDownloader()
-    yd.search(search_query='test')
-
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    # query a download operation
+    yd.download_best_resolution()
