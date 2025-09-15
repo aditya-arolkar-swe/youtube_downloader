281fd948591b59b2f4b6bb9c8a6ee17d7624a1e0
Aditya Arolkar <aditya.arolkar@berkeley.edu>
2025-09-15 05:33:13 -0400

    refactor

--- utils.py
+++ utils.py
@@ -1,18 +1,6 @@
+"""
+Legacy utils module - imports from the new modular structure
+"""
+from src.utils.helpers import user_allows, enforce_options, print_dict, strip_non_ascii, clear_cache
 
-from json import dumps
-
-
-def print_dict(text):
-    print(dumps(text, indent=4))
-
-
-def enforce_options(options):
-    user_input = input(f'Please enter one of {options}:\n')
-    while user_input not in options:
-        user_input = input(f'Please enter one of {options}:\n')
-    return user_input
-
-
-def user_allows(question: str = None):
-    print(question)
-    return 'y' == enforce_options(['y', 'n'])
+__all__ = ['user_allows', 'enforce_options', 'print_dict', 'strip_non_ascii', 'clear_cache']
