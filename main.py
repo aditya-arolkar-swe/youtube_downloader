aa99169303f848636c2f4563789f565b960c36c7
Aditya Arolkar <aditya.arolkar@berkeley.edu>
2025-09-15 05:36:57 -0400

    update gitignore

--- main.py
+++
@@ -1,4 +0,0 @@
-from pytube import YouTube
-
-yt = YouTube('http://youtube.com/watch?v=2lAe1cqCOXo')
-yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first().download()
--- utils.py
+++
@@ -1,6 +0,0 @@
-"""
-Legacy utils module - imports from the new modular structure
-"""
-from src.utils.helpers import user_allows, enforce_options, print_dict, strip_non_ascii, clear_cache
-
-__all__ = ['user_allows', 'enforce_options', 'print_dict', 'strip_non_ascii', 'clear_cache']
